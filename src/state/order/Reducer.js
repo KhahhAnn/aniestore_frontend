355ae96a4368728f9aeda2c75334ceaff57caf33
khahhan <khanhanbui1010@gmail.com>
2024-03-17 13:22:20 +0700

    add action actionType reducer for product, order, cart

---
+++ src/state/order/Reducer.js
@@ -0,0 +1,30 @@
+import { GET_TO_CART_REQUEST } from "../cart/ActionType";
+import { CREATE_ORDER_FAILURE, CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, GET_ORDER_BY_ID_FAILURE, GET_ORDER_BY_ID_SUCCESS } from "./ActionType";
+
+const initialState = {
+   orders: [],
+   order: null,
+   error: null,
+   loading: false,
+}
+
+export const orderReducer = (state = initialState, action) => {
+   switch (action.type) {
+      case GET_TO_CART_REQUEST:
+         return {...state, loading: true, error: null};
+      case GET_ORDER_BY_ID_SUCCESS:
+         return {...state, loading: false, order: action.payload, error: null};
+      case GET_ORDER_BY_ID_FAILURE:
+         return {...state, loading:false, error: action.payload};
+      case CREATE_ORDER_REQUEST:
+         return {...state, loading: true, error: null};
+      case CREATE_ORDER_SUCCESS:
+         return {...state, loading: false, order: action.payload, error: null};
+      case CREATE_ORDER_FAILURE:
+         return {...state, loading:false, error: action.payload};
+
+   
+      default:
+         return state;
+   }
+}
